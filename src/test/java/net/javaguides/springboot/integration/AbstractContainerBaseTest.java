49d3146904290068711f94b6f969dce3ab1507e7
Duke Dang <duc.dangtrong@orientsoftware.com>
2023-03-28 08:30:14 +0700

    Spring Boot Test Container

---
+++ src/test/java/net/javaguides/springboot/integration/AbstractContainerBaseTest.java
@@ -0,0 +1,33 @@
+package net.javaguides.springboot.integration;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+public abstract class AbstractContainerBaseTest {
+
+    static final MySQLContainer MY_SQL_CONTAINER;
+
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest")
+                .withUsername("username")
+                .withPassword("password")
+                .withDatabaseName("ems");
+
+        MY_SQL_CONTAINER.start();
+
+    }
+    git init
+    git add .
+    git commit -m "Spring Boot Test Container"
+    git branch -M main
+    git remote add origin https://github.com/ducdang91/testing-spring-boot.git
+    git push -u origin main
+    @DynamicPropertySource
+    public static void dynamicPropertySource(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+    }
+}
